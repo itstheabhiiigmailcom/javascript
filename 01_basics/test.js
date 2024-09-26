69815e7cd938c2417e512a8eece174d4e932e399
itstheabhiiigmailcom <154773883+itstheabhiiigmailcom@users.noreply.github.com>
2024-09-26 11:40:36 +0000

    test.js variable part completed

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const accountId = 24234
+let accountMail = "itstheabhii@gmail.com"
+var accountPassword = "2434"
+accountCity = "Mumbai"
+let accountState;           // undefined
+
+// accountId = 20                  // not allowed to change const
+accountMail = "some@gmail.com"
+accountPassword = 423423
+accountCity = "Bengaluru"
+
+
+/*
+prefer not to use var because of issue in block and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountMail, accountPassword, accountCity, accountState])
